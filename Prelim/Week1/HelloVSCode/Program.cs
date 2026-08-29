5e11d5c490c8a8f90f373720fe0892dbd0289ebe
unknown <YY915@ccs.local>
2026-08-29 12:56:34 +0800

    Fix Week1 HelloVSCode project

---
+++ Prelim/Week1/HelloVSCode/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello, Applications Development!");
+Console.WriteLine("This is my first C# application using VS Code.");
+Console.WriteLine($"Date and Time: {DateTime.Now}");
+Console.WriteLine("Hello World!");
